e3f632705c67b573cc4fe2dc3d643307b000e09f
Martin Zimmermann <martin.zimmermann.git@outlook.com>
2020-11-23 23:14:34 +0100

    Added JackPackBar

--- Project Files/Scripts/HUDHealthbar.gd
+++ Project Files/Scripts/HUDHealthbar.gd
@@ -1,10 +1,16 @@
 extends Control
 
 onready var healthbarFill : TextureRect = $HealthbarFill
+onready var jetPackbarFill : TextureRect = $JetPackbarFill
+
 var stats = PlayerStats
 
 func set_health_percent(percent):
 	healthbarFill.rect_size.x = int(lerp(0, 64, percent))
 	
+func set_jet_pack_percent(percent):
+	jetPackbarFill.rect_size.x = int(lerp(0, 64, percent))
+	
 func _ready():
 	stats.connect("health_changed", self, "set_health_percent")
+	stats.connect("jetPack_changed", self, "set_jet_pack_percent")
